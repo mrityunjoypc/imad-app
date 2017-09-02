8eb1faa50dbb7ab21e60e6190ec5b9ab875eb9e0
Mrityunjoy PC <mrityunjoy.pc@gmail.com>
2017-09-02 10:31:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,14 @@ console.log('Loaded!');
 //This is to change the text inside the HTML page.
 
 var element = document.getElementById("text-header");
-element.innerHTML = "This text is changed through JavaScript!!"
+element.innerHTML = "This text is changed through JavaScript!!";
+
+//Play around with the image css
+
+var img = document.getElementById('img-logo');
+img.onclick() = function() {
+    img.style.background-color = 'blue';
+    img.style.border = 'ridge';
+    
+    
+};
