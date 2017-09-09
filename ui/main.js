26ac1ae3f07b10ab1ed0018d5e20905128f6a515
Mrityunjoy PC <mrityunjoy.pc@gmail.com>
2017-09-10 01:09:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,14 +24,3 @@ image.onclick = function() {
     var interval = setInterval(moveRight,50);
 };
 
-
-var counter = 0;
-var button = document.getElementById('countBut');
-button.onclick = function(){
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
-    
-};
