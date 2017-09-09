49419f269638fc0749e1073e94d06497fe64b89c
Mrityunjoy PC <mrityunjoy.pc@gmail.com>
2017-09-10 01:34:02 +0530

    [imad-console] Updates ui/JsExample.js

--- ui/JsExample.js
+++ ui/JsExample.js
@@ -10,3 +10,23 @@ button.onclick = function(){
 };
 
 
+var button2 = document.getElementById('countBut2');
+button2.onclick = function(){
+//create request object
+var request = new XMLHttpRequest();
+
+//capture response and store it in a variable
+request.onreadystatechange = function(){
+                                        if (request.readyState == XMLHttpRequest.DONE){
+                                                                                if (request.status == 200){
+                                                                                                var counter2 = request.responseText;
+                                                                                                var span = document.getElementById('count2');
+                                                                                                span.innerHTML = counter2.toString();
+                                                                                                            }
+                                                                                        }
+                                        };
+
+request.open('GET','http://mrityunjoypc.imad.hasura-app.io/ui/counter', true);
+request.send(null);
+
+};
