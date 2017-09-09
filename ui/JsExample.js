a223d30e7dec352cfe0c54dc9fb2effa3fbe5e8c
Mrityunjoy PC <mrityunjoy.pc@gmail.com>
2017-09-10 01:23:27 +0530

    [imad-console] Updates ui/JsExample.js

--- ui/JsExample.js
+++ ui/JsExample.js
@@ -7,5 +7,12 @@ button.onclick = function(){
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
     
-    
 };
+
+
+var counter2 = 0 // this is to initialize the counter
+app.get('/ui/counter', function (req, res) {
+  counter2 = counter2 + 1;
+  res.send(counter2.toString());
+  
+});
