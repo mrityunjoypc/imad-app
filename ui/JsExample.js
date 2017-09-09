94cc3c1e666f2d2b9ef84d16672cab17ade5aef4
Mrityunjoy PC <mrityunjoy.pc@gmail.com>
2017-09-10 00:46:05 +0530

    [imad-console] Updates ui/JsExample.js

--- ui/JsExample.js
+++ ui/JsExample.js
@@ -1,6 +1,6 @@
 var counter = 0 // this is to initialize the counter
 
-app.get('/ui/counter', function (req, res) {
+app.get('/counter', function (req, res) {
   counter = counter + 1;
   res.send(counter.toString());
   
