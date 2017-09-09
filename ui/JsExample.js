d53d99ddddb169a67b4ed2bedef7103e6057cfc2
Mrityunjoy PC <mrityunjoy.pc@gmail.com>
2017-09-10 01:21:37 +0530

    [imad-console] Updates ui/JsExample.js

---
+++ ui/JsExample.js
@@ -0,0 +1,11 @@
+
+var counter = 0;
+var button = document.getElementById('countBut');
+button.onclick = function(){
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+    
+};
