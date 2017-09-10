a324f8e991c0915d25cf07cf85680f80ba83100e
Mrityunjoy PC <mrityunjoy.pc@gmail.com>
2017-09-10 12:19:57 +0530

    [imad-console] Updates ui/task.js

--- ui/task.js
+++ ui/task.js
@@ -1,1 +1,16 @@
 
+var nameInp = documen.getElementById('inp');
+var name = nameInp.value;
+submit.onclick = function(){
+
+
+
+var names = ['name1','name2','name3'];
+var list = '';
+for(var i =0; i<names.length;i++){
+	list+= '<li>'+ names[i] + '</li>';
+}
+var ul = document.getElementById('namelist')
+ul.innerHTML = list;
+};
+
